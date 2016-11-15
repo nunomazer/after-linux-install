ff3c0ea2de480a6a2691098db65fafe073728fbe
Ademir Mazer Jr [ Nuno ] <ademir.mazer.jr@gmail.com>
2016-11-15 12:25:36 -0200

    Duplicated Digikam

--- linux-config-after-install.sh
+++ linux-config-after-install.sh
@@ -228,9 +228,6 @@ sudo -E dpkg -i robomongo-0.8.5-x86_64.deb
 # Unity lens Calculator utility
 #sudo apt-get install unity-lens-utilities/ unity-scope-calculator
 
-# Digikam
-sudo apt-get install digikam
-
 # Mediabuntu
 sudo apt-get install ubuntu-restricted-extras non-free-codecs libdvdcss2 faac faad ffmpeg ffmpeg2theora flac icedax id3v2 lame libflac++6 libjpeg-progs libmpeg3-1 mencoder mjpegtools mp3gain mpeg2dec mpeg3-utils mpegdemux mpg123 mpg321 regionset sox uudeview vorbis-tools x264 arj lha p7zip p7zip-full p7zip-rar rar unrar unace-nonfree vlc
 
